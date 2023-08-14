4b37c750e3f09f5a208ca5334a05ce22f28d7a47
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-08-14 14:43:35 -0300

    Instalação do Vuetify e MDI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">
+      <v-icon icon="mdi-home" />
+      Home
+    </router-link> |
+    
+    <router-link to="/about">
+      <v-icon icon="mdi-help-circle" />
+      About
+    </router-link>
   </nav>
   <router-view/>
 </template>
